593ba8b1926d117dbad7908a236c4a31fcc15448
Ronak Patel <ronakp991@gmail.com>
2018-05-19 20:02:57 -0400

    Register Page Ready

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       home: new LoginPage(),
-      theme: new ThemeData(
-        primarySwatch: Colors.blue
-      ),
+      theme: new ThemeData(primarySwatch: Colors.blue),
     );
   }
 }
@@ -19,28 +17,100 @@ class LoginPage extends StatefulWidget {
   State createState() => new LoginPageState();
 }
 
-class LoginPageState extends State<LoginPage>{
-
-  //Animation is not need as of now.
-
-  /*AnimationController animationController;
-  Animation<double> animation;
-
+class RegisterPage extends StatefulWidget {
   @override
-  void initState() {
-    super.initState();
-    animationController = new AnimationController(
-        vsync: this,
-      duration: new Duration(milliseconds: 300)
-    );
-    animation = new CurvedAnimation(
-        parent: animationController,
-        curve: Curves.easeOut
-    );
-    animation.addListener(() => this.setState((){}));
-    animationController.forward();
-  }*/
+  State createState() => new RegisterPageState();
+}
 
+class RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.green,
+      body: new Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          new Image(
+            image: new AssetImage("assets/Encyferz.jpg"),
+            fit: BoxFit.cover,
+            color: Colors.black87,
+            colorBlendMode: BlendMode.darken,
+          ),
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Form(
+                  child: new Theme(
+                data: new ThemeData(
+                    brightness: Brightness.dark,
+                    primarySwatch: Colors.teal,
+                    inputDecorationTheme: new InputDecorationTheme(
+                        labelStyle:
+                            new TextStyle(color: Colors.teal, fontSize: 20.0))),
+                child: new Container(
+                  padding: const EdgeInsets.all(40.0),
+                  child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      new TextFormField(
+                        decoration: new InputDecoration(
+                          labelText: "First Name",
+                        ),
+                        keyboardType: TextInputType.text,
+                      ),
+                      new TextFormField(
+                        decoration: new InputDecoration(
+                          labelText: "Last Name",
+                        ),
+                        keyboardType: TextInputType.text,
+                      ),
+                      new TextFormField(
+                        decoration: new InputDecoration(
+                          labelText: "Password",
+                        ),
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                      ),
+                      new TextFormField(
+                        decoration: new InputDecoration(
+                          labelText: "Re-Password",
+                        ),
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                      ),
+                      new Padding(
+                        padding: const EdgeInsets.only(top: 20.0),
+                      ),
+                      new MaterialButton(
+                        color: Colors.teal,
+                        textColor: Colors.white,
+                        child: new Text("Register"),
+                        onPressed: () => {},
+                      ),
+                      new Padding(
+                        padding: const EdgeInsets.only(top: 20.0),
+                      ),
+                      new MaterialButton(
+                        color: Colors.teal,
+                        textColor: Colors.white,
+                        child: new Text("Login"),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -58,64 +128,61 @@ class LoginPageState extends State<LoginPage>{
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               new Image(
-                  image: new AssetImage("assets/Encyferz.jpg"),
+                image: new AssetImage("assets/Encyferz.jpg"),
               ),
               new Form(
-                  child: new Theme(
-                    data: new ThemeData(
+                child: new Theme(
+                  data: new ThemeData(
                       brightness: Brightness.dark,
                       primarySwatch: Colors.teal,
                       inputDecorationTheme: new InputDecorationTheme(
-                        labelStyle: new TextStyle(
+                          labelStyle: new TextStyle(
+                              color: Colors.teal, fontSize: 20.0))),
+                  child: new Container(
+                    padding: const EdgeInsets.all(40.0),
+                    child: new Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        new TextFormField(
+                          decoration: new InputDecoration(
+                            labelText: "Email",
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        new TextFormField(
+                          decoration: new InputDecoration(
+                            labelText: "Password",
+                          ),
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                        ),
+                        new Padding(
+                          padding: const EdgeInsets.only(top: 20.0),
+                        ),
+                        new MaterialButton(
                           color: Colors.teal,
-                          fontSize: 20.0
-                        )
-                      )
-                    ),
-                    child: new Container(
-                      padding: const EdgeInsets.all(40.0),
-                      child: new Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          new TextFormField(
-                            decoration: new InputDecoration(
-                              labelText: "Email",
-                            ),
-                            keyboardType: TextInputType.emailAddress,
-                          ),
-                          new TextFormField(
-                            decoration: new InputDecoration(
-                              labelText: "Password",
-                            ),
-                            keyboardType: TextInputType.text,
-                            obscureText: true,
-                          ),
-                          new Padding(
-                              padding: const EdgeInsets.only(
-                                top: 20.0
-                              ),
-                          ),
-                          new MaterialButton(
-                            color: Colors.teal,
-                            textColor: Colors.white,
-                            child: new Text("Login"),
-                            onPressed: () => {},
-                          ),
-                          new Padding(
-                            padding: const EdgeInsets.only(
-                                top: 20.0
-                            ),
-                          ),
-                          new MaterialButton(
+                          textColor: Colors.white,
+                          child: new Text("Login"),
+                          onPressed: () => {},
+                        ),
+                        new Padding(
+                          padding: const EdgeInsets.only(top: 20.0),
+                        ),
+                        new MaterialButton(
                             color: Colors.teal,
                             textColor: Colors.white,
                             child: new Text("Register"),
-                            onPressed: () => {},
-                          ),
-                        ],
-                      ),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  new MaterialPageRoute(
+                                      builder: (context) =>
+                                          new RegisterPage()));
+                            }),
+                      ],
                     ),
                   ),
+                ),
               ),
             ],
           ),
